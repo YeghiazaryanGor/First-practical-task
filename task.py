879052a4f50a2cd3a8ed56cfede1e1758a7dcb9b
Gor <gor.yeghiazaryan.fd@gmail.com>
2022-06-28 18:42:37 +0400

    Changing columns filtering algorithm

--- task.py
+++ task.py
@@ -5,32 +5,37 @@ import csv  # Importing csv module to work with data
 N = 10
 fov_h = 60
 fov_v = 70
+cols = ["ra_ep2000","dec_ep2000","b"]
 Data_sample = {}
 with open("337.all.tsv") as file:
     tsv_file = csv.reader(file, delimiter='\t')
     data = list(tsv_file)
 
 
-def cols_used(fl, *columns):  # Selecting the columns that we will use during the task
+def cols_used(fl, columns):  # Selecting the columns that we will use during the task
     for col in columns:
         Data_sample[col] = []
-    for i in range(2, len(fl)):
-        Data_sample[columns[0]].append(float(fl[i][0]))
-        Data_sample[columns[1]].append(float(fl[i][1]))
-        Data_sample[columns[2]].append(float(fl[i][-1]))
+        for i in range(2,len(fl)):
+            Data_sample[col].append(float(fl[i][fl[1].index(col)]))
 
 
-cols_used(data, "RA", "DEC", "b")
+    # for i in range(2, len(fl)):
+    #     Data_sample[columns[0]].append(float(fl[i][0]))
+    #     Data_sample[columns[1]].append(float(fl[i][1]))
+    #     Data_sample[columns[2]].append(float(fl[i][-1]))
+
+
+cols_used(data, cols)
 
 stars_in_fov = {k: [] for k in Data_sample.keys()}
 starID = []
 
 
 def fov_filtering(fov_h, fov_v):  # Selecting the stars that are in our field of view
-    for j in range(len(Data_sample["RA"])):
-        if float(Data_sample["RA"][j]) < fov_h and float(Data_sample["DEC"][j]) < fov_v:
-            stars_in_fov["RA"].append(Data_sample["RA"][j])
-            stars_in_fov["DEC"].append(Data_sample["DEC"][j])
+    for j in range(len(Data_sample["ra_ep2000"])):
+        if float(Data_sample["ra_ep2000"][j]) < fov_h and float(Data_sample["dec_ep2000"][j]) < fov_v:
+            stars_in_fov["ra_ep2000"].append(Data_sample["ra_ep2000"][j])
+            stars_in_fov["dec_ep2000"].append(Data_sample["dec_ep2000"][j])
             stars_in_fov["b"].append(Data_sample["b"][j])
             starID.append(j)
 
